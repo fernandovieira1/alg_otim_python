810ce68861c092344b9927316a18b01082d38d51
Fernando Vieira <fernandovieira1@outlook.com>
2022-02-03 01:03:21 -0400

    vscode mac 03022022 / Impressão do cronograma de voos I

--- otimizacao_voos.py
+++ otimizacao_voos.py
@@ -3,12 +3,13 @@ import random
 import math
 
 pessoas = [('Amanda', 'CWB'),
-          ('Marcos', 'GIG'),
+          ('Pedro', 'GIG'),
+          ('Marcos', 'POA'),
           ('Priscila', 'FLN'),
-          ('Jessica', 'OFN'),
+          ('Jessica', 'CNF'),
           ('Paulo', 'GYN')]
 
-destino = 'GRL'
+destino = 'GRU'
 
 voos = {}
 for linha in open('voos.txt'):
@@ -17,4 +18,20 @@ for linha in open('voos.txt'):
     voos.setdefault((_origem, _destino), [])
     voos[(_origem, _destino)].append((_saida, _chegada, int(_preco)))
 
-print(voos['GRU', 'CWB'])
+# [1,4, 3,2, 7,3, 6,3, 2,4, 5,3]
+# Lista de nrs com seis pessoas e doze posições correspondentes às linhas contendo os vôos que cada pessoa vai pegar
+def imprimir_agenda(agenda):
+    id_voo = -1
+    for i in range(len(agenda) // 2):
+        nome = pessoas[i][0]
+        origem = pessoas[i][1]
+        id_voo += 1
+        ida = voos[(origem, destino)][agenda[id_voo]]
+        id_voo += 1
+        volta = voos[(destino, origem)]
+        print('%10s%10s %5s-%5s R$%3s %5s-%5s R$%3s' % (nome, origem, ida[0], ida[1], ida[2],
+                                                                             volta[0], volta[1], volta[2]))
+
+agenda = [1,4, 3,2, 7,3, 6,3, 2,4, 5,3]
+
+imprimir_agenda(agenda)
