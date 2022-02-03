39f9f8cec571ec2db54d97f19744f973a567dd65
Fernando Vieira <fernandovieira1@outlook.com>
2022-02-03 00:18:20 -0400

    vscode mac 03022022 / setup configurado e otimizacao voos iniciado

---
+++ otimizacao_voos.py
@@ -0,0 +1,11 @@
+import time
+import random
+import math
+
+pessoas = [('Amanda', 'CWB'),
+          ('Marcos', 'GIG'),
+          ('Priscila', 'FLN'),
+          ('Jessica', 'OFN'),
+          ('Paulo', 'GYN')]
+
+destino = 'GRL'
