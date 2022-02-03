e21c8ed9309a0362129451b42832b4e8b4320166
Fernando Vieira <fernandovieira1@outlook.com>
2022-02-03 00:26:22 -0400

    vscode mac 03022022 / base de dados voos

--- otimizacao_voos.py
+++ otimizacao_voos.py
@@ -9,3 +9,12 @@ pessoas = [('Amanda', 'CWB'),
           ('Paulo', 'GYN')]
 
 destino = 'GRL'
+
+voos = {}
+for linha in open('voos.txt'):
+    #print(linha)
+    _origem, _destino, _saida, _chegada, _preco = linha.split(',') # '_' para deixar as variáveis como locais (apenas no 'for')
+    voos.setdefault((_origem, _destino), [])
+    voos[(_origem, _destino)].append((_saida, _chegada, int(_preco)))
+
+print(voos['GRU', 'CWB'])
