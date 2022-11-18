a5c05eeef42b2fda6acb4b99992d8189e23e11e4
Fernando Vieira <fernandovieira1@outlook.com>
2022-11-18 13:50:01 -0400

    funcao get minutos - vs mac - 18112022

--- otimizacao_voos.py
+++ otimizacao_voos.py
@@ -13,23 +13,23 @@ pessoas = [('Amanda', 'CWB'),
 # Local de destino
 destino = 'GRU'
 
-## Montar o dicionário de todas as combinações possíveis de voos (chaves)
+## Montar o dicionario de todas as combinacões possiveis de voos (chaves)
 voos = {}
 for linha in open('voos.txt'):
-    _origem, _destino, _saida, _chegada, _preco = linha.split(',') # '_' para deixar as variáveis como locais (apenas no/dentro do 'for')
+    _origem, _destino, _saida, _chegada, _preco = linha.split(',') # '_' para deixar as variaveis como locais (apenas no/dentro do 'for')
     voos.setdefault((_origem, _destino), []) # define o item com as chaves especificadas
-    voos[(_origem, _destino)].append((_saida, _chegada, int(_preco))) # para cada origem e destino, define as possíveis combinações de saída, chegada e preço 
+    voos[(_origem, _destino)].append((_saida, _chegada, int(_preco))) # para cada origem e destino, define as possiveis combinacões de saida, chegada e preco 
 
 ## Imprimir agenda (cronograma)
-# Forma de representar a solução, no caso, uma lista de números.
+# Forma de representar a solucao, no caso, uma lista de números.
 # [1,4, 3,2, 7,3, 6,3, 2,4, 5,3]
-# Seis pessoas, doze posições.
-# Lista de nrs com seis pessoas e doze posições correspondentes às linhas contendo os vôos que cada pessoa vai pegar.
-# Assim sendo, os vôos da Amanda, Pedro, Marcos... respectivamente. 
-# Ficando mais claro, a Amanda pegará o primeiro vôo (da lista descrita no dicionário) na ida, e o quarto na volta.
+# Seis pessoas, doze posicões.
+# Lista de nrs com seis pessoas e doze posicões correspondentes às linhas contendo os voos que cada pessoa vai pegar.
+# Assim sendo, os voos da Amanda, Pedro, Marcos... respectivamente. 
+# Ficando mais claro, a Amanda pegara o primeiro voo (da lista descrita no dicionario) na ida, e o quarto na volta.
 def imprimir_agenda(agenda):
-    id_voo = -1 # essa variável é usada para percorrer cada uma das linhas do dicionário (voos) (-1 pq será incrementada += e a primeira posição no Python é 0)
-    for i in range(len(agenda) // 2): # se eu tenho seis posições, 
+    id_voo = -1 # essa variavel e usada para percorrer cada uma das linhas do dicionario (voos) (-1 pq sera incrementada += e a primeira posicao no Python e 0)
+    for i in range(len(agenda) // 2): # se eu tenho seis posicões, 
         nome = pessoas[i][0] # primeira coluna
         origem = pessoas[i][1] # segunda coluna
         id_voo += 1
@@ -39,6 +39,14 @@ def imprimir_agenda(agenda):
         print('%10s%10s %5s-%5s R$%3s %5s-%5s R$%3s' % (nome, origem, ida[0], ida[1], ida[2],
                                                                              volta[0], volta[1], volta[2]))
 
-agenda = [1,4, 3,2, 7,3, 6,3, 2,4, 5,3]
+agenda = [1,4, 3,2, 7,3, 6,3, 2,4, 5,3] # nao entendi direito isso aqui
 
 imprimir_agenda(agenda)
+
+## Conversao em minutos do tempo (em horas)
+def get_minutos(hora):
+    x = time.strptime(hora, '%H:%M')
+    minutos = x[3] * 60 + x[4]
+    return minutos
+
+get_minutos('6:13')
